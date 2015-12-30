66456910ced96812c92b61723e93a92026a94fc6
Stuart Blair <stuart.a.blair@gmail.com>
2015-12-29 19:51:50 -0800

    Adding The Mummy!

---
+++ src/main/java/com/github/lachlanblair/entity/EntityMummy.java
@@ -0,0 +1,11 @@
+package com.github.lachlanblair.entity;
+
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.world.World;
+
+public class EntityMummy extends EntityMob
+{
+    public EntityMummy(World worldIn) {
+        super(worldIn);
+    }
+}
--- src/main/java/com/github/lachlanblair/proxy/ClientProxy.java
+++ src/main/java/com/github/lachlanblair/proxy/ClientProxy.java
@@ -1,8 +1,10 @@
 package com.github.lachlanblair.proxy;
 
+import com.github.lachlanblair.entity.EntityMummy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 public class ClientProxy extends CommonProxy {
 
@@ -14,6 +16,7 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void init(FMLInitializationEvent e) {
         super.init(e);
+        EntityRegistry.registerGlobalEntityID(EntityMummy.class, "Mummy", EntityRegistry.findGlobalUniqueEntityId(), 230, 78);
     }
 
     @Override
