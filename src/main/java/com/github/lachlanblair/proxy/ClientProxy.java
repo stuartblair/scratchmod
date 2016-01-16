427166beab8039ea0e4adddf7c5693277b1d5766
Stuart Blair <stuart.a.blair@gmail.com>
2016-01-16 15:19:20 -0800

    Adding The Mummy!

--- src/main/java/com/github/lachlanblair/proxy/ClientProxy.java
+++ src/main/java/com/github/lachlanblair/proxy/ClientProxy.java
@@ -1,10 +1,14 @@
 package com.github.lachlanblair.proxy;
 
+import com.github.lachlanblair.Blakes7Mod;
 import com.github.lachlanblair.entity.EntityMummy;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.common.registry.LanguageRegistry;
 
 public class ClientProxy extends CommonProxy {
 
@@ -17,6 +21,7 @@ public class ClientProxy extends CommonProxy {
     public void init(FMLInitializationEvent e) {
         super.init(e);
         EntityRegistry.registerGlobalEntityID(EntityMummy.class, "Mummy", EntityRegistry.findGlobalUniqueEntityId(), 230, 78);
+        EntityRegistry.addSpawn(EntityMummy.class, 1, 1, 1000, EnumCreatureType.CREATURE, BiomeGenBase.desert);
     }
 
     @Override
--- src/main/java/com/github/lachlanblair/proxy/CommonProxy.java
+++ src/main/java/com/github/lachlanblair/proxy/CommonProxy.java
@@ -3,6 +3,7 @@ package com.github.lachlanblair.proxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 public class CommonProxy {
 
